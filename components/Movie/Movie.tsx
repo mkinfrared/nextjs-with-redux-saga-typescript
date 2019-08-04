51af6b3493e64105cacdc123a5742ffeea4deadc
mkinfrared <mkinfrared@gmail.com>
2019-08-04 19:33:07 +0300

    feat(#2): Absolute imports
    * Added absolute imports

--- components/Movie/Movie.tsx
+++ components/Movie/Movie.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { MovieProps, MovieState } from "./Movie.type";
-import movieApi from "../../utils/movieApi";
+import movieApi from "utils/movieApi";
 
 import css from "./Movie.scss";
 
